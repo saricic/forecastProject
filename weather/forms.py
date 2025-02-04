aa16f9bde25af0fa775ccd1b3799a73a0753978f
saricic <saricicekserkann@gmail.com>
2025-02-04 20:46:47 +0100

    style improved

--- weather/forms.py
+++ weather/forms.py
@@ -5,16 +5,17 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Explicitly set widget id attributes so labels reference valid ids
-        self.fields['username'].widget.attrs.update({'id': 'id_username'})
-        self.fields['password1'].widget.attrs.update({'id': 'id_password1'})
-        self.fields['password2'].widget.attrs.update({'id': 'id_password2'})
 
-        # Override the default help texts
+        # Widget'lara özel CSS sınıfları ekleme
+        self.fields['username'].widget.attrs.update({'id': 'id_username', 'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'id': 'id_password1', 'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'id': 'id_password2', 'class': 'form-control'})
+
+        # Help text'leri özelleştirme
         self.fields['username'].help_text = "Your username can only include letters and numbers."
         self.fields[
             'password1'].help_text = "Your password must be at least 8 characters long and not entirely numeric."
-        self.fields['password2'].help_text = "Please enter the same password as above."
+        self.fields['password2'].help_text = "Enter the password again."
 
     class Meta:
         model = User
